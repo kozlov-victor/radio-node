0cc0a1a1cf0d2d8d6631ba5c0404ee8d2606a65f
Kozlov Victor <kozlovvictorzx@gmail.com>
2021-08-09 19:50:12 +0300

    some experiments with nextStation()

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const nextStation = ()=>{
     if (player) {
         player.removeListener('error',onError);
         player.removeListener('close',onClose);
-        player.quit();
+        if (player.running) player.quit();
         clearTimeout(tidClose);
         clearTimeout(tidError);
         runPlayer();
